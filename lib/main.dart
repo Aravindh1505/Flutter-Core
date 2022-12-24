15a3d0db8562fbb35bea82509499b5055a958c47
Aravindh1505 <aravindh.samidurai@gmail.com>
2022-12-24 20:33:14 +0530

    New: Widget styling tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -6,37 +6,13 @@ import 'package:flutter_core/utils/utils.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
-  @override
   Widget build(BuildContext context) {
-    //Size in physical pixels
-    var physicalScreenSize = window.physicalSize;
-    var width = physicalScreenSize.width;
-    var height = physicalScreenSize.height;
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Smart Illam'),
-        ),
-        body: Container(
-          width: width,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(
-                onPressed: () {
-
-                },
-                child: const Text('Click'),
-              )
-            ],
-          ),
+          title: const Text('Widget Styling'),
         ),
       ),
     );
