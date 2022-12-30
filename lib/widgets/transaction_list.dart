12d3a16680cff5fe44d232598b3ebc02579c5ab6
Aravindh1505 <aravindh.samidurai@gmail.com>
2022-12-30 12:38:12 +0530

    Charbar UI updated

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -12,7 +12,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 600,
+      height: 500,
       child: transactions.isEmpty
           ? Container(
               margin: const EdgeInsets.all(50.0),
@@ -23,44 +23,6 @@ class TransactionList extends StatelessWidget {
             )
           : ListView.builder(
               itemBuilder: (context, index) {
-                /* return SizedBox(
-                  width: double.infinity,
-                  height: 100,
-                  child: Card(
-                    elevation: 5.0,
-                    child: Container(
-                      margin: const EdgeInsets.all(10.0),
-                      child: Row(
-                        children: [
-                          Container(
-                            width: 50.0,
-                            margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
-                            child: Text('\$ ${transactions[index].amount.toStringAsFixed(2)}',
-                                style: TextStyle(color: Theme.of(context).primaryColor)),
-                          ),
-                          Container(
-                            margin: const EdgeInsets.only(left: 15.0),
-                            padding: const EdgeInsets.all(5.0),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  transactions[index].title,
-                                  style: Theme.of(context).textTheme.titleLarge,
-                                ),
-                                Text(
-                                  DateFormat().format(transactions[index].date),
-                                  style: const TextStyle(fontSize: 12.0, fontWeight: FontWeight.w300),
-                                )
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                );*/
                 return ListTile(
                   leading: CircleAvatar(
                     radius: 30.0,
