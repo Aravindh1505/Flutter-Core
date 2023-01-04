923295913359b1be30d9b9903cbfe8ef311a8034
Aravindh1505 <aravindh.samidurai@gmail.com>
2023-01-04 16:02:11 +0530

    Added adaptive ui to support both Android & iOS platform.

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,51 @@
-import 'dart:ui';
+import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_core/utils/utils.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    //Size in physical pixels
-    var physicalScreenSize = window.physicalSize;
-    var width = physicalScreenSize.width;
-    var height = physicalScreenSize.height;
+    return Platform.isAndroid ? buildMaterialApp() : buildCupertinoApp();
+  }
 
+  MaterialApp buildMaterialApp() {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Smart Illam'),
+          title: const Text('Smart Home'),
         ),
-        body: Container(
-          width: width,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(
-                onPressed: () {
-
-                },
-                child: const Text('Click'),
-              )
-            ],
-          ),
-        ),
+        body: buildBody,
       ),
     );
   }
+
+  CupertinoApp buildCupertinoApp() {
+    return CupertinoApp(
+      home: CupertinoPageScaffold(
+        navigationBar: const CupertinoNavigationBar(
+          middle: Text('Smart Home'),
+        ),
+        child: buildBody,
+      ),
+    );
+  }
+
+  final buildBody = Column(
+    children: [
+      Platform.isIOS
+          ? CupertinoButton(
+              onPressed: () {},
+              child: Text('Submit'),
+            )
+          : ElevatedButton(
+              onPressed: () {},
+              child: Text('Submit'),
+            ),
+    ],
+  );
 }
