1c47854e045f11950ddcf5d93430dfbc3721a921
Aravindh1505 <aravindh.samidurai@gmail.com>
2023-01-07 13:53:46 +0530

    Cleanup: Removed toast and utils classes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_core/utils/utils.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,10 +9,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //Size in physical pixels
-    var physicalScreenSize = window.physicalSize;
-    var width = physicalScreenSize.width;
-    var height = physicalScreenSize.height;
 
     return MaterialApp(
       home: Scaffold(
@@ -21,7 +16,6 @@ class MyApp extends StatelessWidget {
           title: const Text('Smart Illam'),
         ),
         body: Container(
-          width: width,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.center,
--- lib/utils/utils.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/foundation.dart';
-
-class Utils {
-   static void printLog(String msg) {
-    if (kDebugMode) {
-      print(msg);
-    }
-  }
-}
