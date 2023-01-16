f6ee9e94f60ef2019383218fac02c0e0dd8c5327
Aravindh1505 <aravindh.samidurai@gmail.com>
2023-01-16 20:11:38 +0530

    Meal item updated

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -24,15 +24,62 @@ class MealItem extends StatelessWidget {
             Stack(
               children: [
                 ClipRRect(
-                  borderRadius: const BorderRadius.all(Radius.circular(15.0)),
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(15.0),
+                    topRight: Radius.circular(15.0),
+                  ),
                   child: Image.network(
                     meal.imageUrl,
                     height: 250,
                     width: double.infinity,
                     fit: BoxFit.cover,
                   ),
-                )
+                ),
+                Positioned(
+                  bottom: 15.0,
+                  right: 0,
+                  child: Container(
+                    width: 250,
+                    color: Colors.black54,
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text(
+                      meal.title,
+                      style: const TextStyle(fontSize: 25, color: Colors.white),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                ),
               ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    children: [
+                      const Icon(Icons.schedule),
+                      const SizedBox(width: 5.0),
+                      Text('${meal.duration} min'),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      const Icon(Icons.work),
+                      const SizedBox(width: 5.0),
+                      Text(meal.complexity.name),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      const Icon(Icons.attach_money),
+                      const SizedBox(width: 5.0),
+                      Text(meal.affordability.name),
+                    ],
+                  )
+                ],
+              ),
             )
           ],
         ),
