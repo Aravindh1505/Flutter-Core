111612d53b53a0186aa0b71d4876e2e1338b6930
Aravindh1505 <aravindh.samidurai@gmail.com>
2023-01-18 15:26:31 +0530

    Meal details screen - Ingredients and Steps UI updated

--- lib/screens/meal_details_screen.dart
+++ lib/screens/meal_details_screen.dart
@@ -21,39 +21,66 @@ class MealDetailScreen extends StatelessWidget {
               width: double.infinity,
               fit: BoxFit.cover,
             ),
-            buildTile(context, 'Ingredients', meal.ingredients),
-            buildTile(context, 'Steps', meal.steps),
+            buildTile(
+              context,
+              'Ingredients',
+              ListView.builder(
+                itemBuilder: (ctx, index) {
+                  return Container(
+                    margin: EdgeInsets.all(5.0),
+                    padding: EdgeInsets.all(2.0),
+                    child: Text(
+                      '* ${meal.ingredients[index]}',
+                      style: Theme.of(context).textTheme.bodySmall,
+                    ),
+                  );
+                },
+                itemCount: meal.ingredients.length,
+              ),
+            ),
+            buildTile(
+              context,
+              'Steps',
+              ListView.builder(
+                itemBuilder: (ctx, index) {
+                  return Column(
+                    children: [
+                      ListTile(
+                        leading: CircleAvatar(
+                          child: Text('#${index + 1}'),
+                        ),
+                        title: Text(meal.steps[index]),
+                      ),
+                      const Divider(thickness: 1.0),
+                    ],
+                  );
+                },
+                itemCount: meal.steps.length,
+              ),
+            ),
+            //buildTile(context, 'Steps', meal.steps),
           ],
         ),
       ),
     );
   }
 
-  Widget buildTile(BuildContext context, String title, List<String> items) {
+  Widget buildTile(BuildContext context, String title, Widget child) {
     return Column(
       children: [
         const SizedBox(height: 10),
         Text(title, style: Theme.of(context).textTheme.titleLarge),
         const SizedBox(height: 10),
         Container(
-          margin: const EdgeInsets.all(5.0),
-          padding: const EdgeInsets.all(5.0),
-          height: 150,
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.grey, width: 2.0),
-            borderRadius: BorderRadius.circular(10.0),
-            color: Colors.white,
-          ),
-          child: ListView.builder(
-            itemBuilder: (ctx, index) {
-              return Text(
-                items[index],
-                style: Theme.of(context).textTheme.bodySmall,
-              );
-            },
-            itemCount: items.length,
-          ),
-        )
+            margin: const EdgeInsets.all(5.0),
+            padding: const EdgeInsets.all(5.0),
+            height: 200,
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.pinkAccent, width: 2.0),
+              borderRadius: BorderRadius.circular(10.0),
+              color: Colors.white,
+            ),
+            child: child)
       ],
     );
   }
