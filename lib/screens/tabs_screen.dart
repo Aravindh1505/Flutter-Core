6fcfacf8c733512ee8f2834afddd6b70c6c033c5
Aravindh1505 <aravindh.samidurai@gmail.com>
2023-01-19 12:22:10 +0530

    Tabs bar changed into bottom navigation bar

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -11,32 +11,45 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, Object>> _pages = [
+    {
+      'page': const CategoriesScreen(),
+      'title': 'Categories',
+    },
+    {
+      'page': const FavoriteScreen(),
+      'title': 'Favorites',
+    }
+  ];
+
+  int _selectedIndex = 0;
+
+  void selectedItem(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text('Meals App'),
-            bottom: const TabBar(
-              tabs: [
-                Tab(
-                  icon: Icon(Icons.category),
-                  text: 'Category',
-                ),
-                Tab(
-                  icon: Icon(Icons.favorite),
-                  text: 'Favorites',
-                ),
-              ],
-            ),
-          ),
-          body: const TabBarView(
-            children: [
-              CategoriesScreen(),
-              FavoriteScreen(),
-            ],
-          ),
-        ));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedIndex]['title'] as String),
+      ),
+      body: _pages[_selectedIndex]['page'] as Widget,
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.black54,
+        currentIndex: _selectedIndex,
+        onTap: selectedItem,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.category), label: _pages[0]['title'] as String),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite), label: _pages[1]['title'] as String),
+        ],
+      ),
+    );
   }
 }
